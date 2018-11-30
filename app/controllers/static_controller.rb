3874032c6a3556f5724ee1d8d31e6585a4512480
Ernest Walzel <ewalzel@gmail.com>
2018-11-30 21:38:35 +0100

    Add something to index

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,5 @@
+class HealthController < ApplicationController
+  def index
+    render html: 'Návody.digital!'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'static#index'
+
   get :health, to: 'health#index'
 end
