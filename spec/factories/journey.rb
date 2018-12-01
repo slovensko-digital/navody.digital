f89701c92cf585a267130a15421afab51abd572b
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2018-12-01 15:33:04 +0100

    Fix Journey and Step factories (pt. 2)

--- spec/factories/journey.rb
+++ spec/factories/journey.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :journey do
-    title { '%s %d' % [Faker::Commerce.product_name, rand(1..1000)] }
+    sequence(:title) do |n|
+      '%s %d' % [Faker::Commerce.product_name, n]
+    end
     keywords { Faker::Commerce.department(5).gsub(/,/, '') }
     published_status { 'PUBLISHED' }
     slug { title.parameterize }
--- spec/factories/step.rb
+++ spec/factories/step.rb
@@ -2,7 +2,9 @@ FactoryBot.define do
   factory :step do
     association :journey
 
-    title { '%s %d' % [Faker::Commerce.product_name, rand(1..1000)] }
+    sequence(:title) do |n|
+      '%s %d' % [Faker::Commerce.product_name, n]
+    end
     keywords { Faker::Commerce.department(5).gsub(/,/, '') }
     is_waiting_step { false }
     slug { title.parameterize }
