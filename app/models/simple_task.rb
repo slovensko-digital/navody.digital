ca65926a576466497f15be13d56163e9269cc79d
Miroslav Hettes <mirrec@gmail.com>
2018-12-01 13:16:22 +0100

    Add data for zivnost

---
+++ app/models/simple_task.rb
@@ -0,0 +1,2 @@
+class SimpleTask < Task
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_01_114726) do
+ActiveRecord::Schema.define(version: 2018_12_01_121130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2018_12_01_114726) do
     t.string "type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "url"
     t.index ["step_id"], name: "index_tasks_on_step_id"
   end
 
