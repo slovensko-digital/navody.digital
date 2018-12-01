727ce1256f05f64dad754b8711b8cfabfa6e397d
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2018-12-01 14:30:36 +0100

    Refactor Admin::AdminController authentication to yaml config

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,3 +1,6 @@
 class Admin::AdminController < ApplicationController
-  http_basic_authenticate_with name: ENV.fetch('ADMIN_USERNAME', 'admin'), password: ENV.fetch('ADMIN_PASSWORD', 'admin')
+  http_basic_authenticate_with(
+      name: Rails.application.config_for(:app).dig('admin', 'username'),
+      password: Rails.application.config_for(:app).dig('admin', 'password'),
+  )
 end
