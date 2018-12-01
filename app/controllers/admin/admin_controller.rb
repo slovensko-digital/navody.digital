5fe8219ce787d2ed195fd7372649bb75aecf14be
Valter Martinek <martinek@freevision.sk>
2018-12-01 14:57:38 +0100

    Use custom template for admin.

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,6 @@
 class Admin::AdminController < ApplicationController
+  layout 'admin'
+
   http_basic_authenticate_with(
       name: Rails.application.config_for(:app).dig('admin', 'username'),
       password: Rails.application.config_for(:app).dig('admin', 'password'),
