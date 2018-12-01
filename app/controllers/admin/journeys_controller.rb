895cedeeaa374b1bd370d4e262880eeda71a7e44
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2018-12-01 13:18:54 +0100

    Add Admin::JourneyController#show action

--- app/controllers/admin/journeys_controller.rb
+++ app/controllers/admin/journeys_controller.rb
@@ -2,4 +2,10 @@ class Admin::JourneysController < Admin::AdminController
   def index
     @journeys = Journey.all
   end
+
+  def show
+    journey_id = params.require(:id)
+
+    @journey = Journey.find(journey_id)
+  end
 end
