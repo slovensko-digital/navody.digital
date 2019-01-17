f20e9eb2192ffcabde9457482c1f8a35de4a1e13
Ernest Walzel <ewalzel@gmail.com>
2019-01-17 18:04:28 +0100

    Add basic search results page Includes a panel to suggest content.
    Fixes #25
    Relates to #19

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,5 +5,7 @@ class SearchesController < ApplicationController
 
     @journeys = Journey.where("search_terms @@ to_tsquery(?)", analyzed_q)
     @steps = Step.where("search_terms @@ to_tsquery(?)", analyzed_q)
+
+    @count = @journeys.count + @steps.count
   end
 end
