2cae9b89e70bad0eb33601d7cf2b0cdaf616ff4b
Ernest Walzel <ewalzel@gmail.com>
2019-01-17 17:07:44 +0100

    Add rails-i18n. Add sk locale

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.5.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1', '>= 5.2.1.1'
+gem 'rails-i18n'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module NavodySlovenskoDigital
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.i18n.default_locale = :sk
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
