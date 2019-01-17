4823bf5b50f38493c3c81d4df4cbe0f23f29a5a3
Ernest Walzel <ewalzel@gmail.com>
2019-01-17 15:36:44 +0100

    Working newsletter sign-up form (pending config) Related to #9

---
+++ app/assets/javascripts/newsletter_sign_up.js
@@ -0,0 +1,26 @@
+$(document).on('ready turbolinks:load', function () {
+  $('#newsletter-form').submit(function (evt) {
+    evt.preventDefault();
+
+    var form = $(evt.target);
+    var data = form.serialize();
+    var button = form.find('button')[0];
+    button.innerHTML = 'Prihlasujem...';
+
+    $.ajax({
+      type: 'POST',
+      url: form.data('action'),
+      data: data,
+      dataType: 'json',
+      success: function (data) {
+        if (data.result !== undefined && data.result.result === 'success') {
+          form.remove();
+          $('#newsletter-success').show();
+        }
+      },
+      complete: function () {
+        button.innerHTML = 'Prihlásiť'
+      }
+    });
+  });
+});
