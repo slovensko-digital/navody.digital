f57ce9f6dc488de9467c84e63273edea87c3623b
jsuchal <jan.suchal@gmail.com>
2019-01-23 12:09:17 +0100

    Support multiword searches

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,9 +3,9 @@ class SearchesController < ApplicationController
     @q = params[:q]
     analyzed_q = Transliterator.transliterate(@q)
 
-    @journeys = Journey.where("search_terms @@ to_tsquery(?)", analyzed_q)
-    @steps = Step.where("search_terms @@ to_tsquery(?)", analyzed_q)
-    @pages = Page.faq.where("search_terms @@ to_tsquery(?)", analyzed_q)
+    @journeys = Journey.where("search_terms @@ plainto_tsquery(?)", analyzed_q)
+    @steps = Step.where("search_terms @@ plainto_tsquery(?)", analyzed_q)
+    @pages = Page.faq.where("search_terms @@ plainto_tsquery(?)", analyzed_q)
 
     @count = @journeys.count + @steps.count + @pages.count
   end
