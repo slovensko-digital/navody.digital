3a4eac88ecea54b2eef9993c53c31ba98210e6fb
Ernest Walzel <ewalzel@gmail.com>
2019-01-23 21:39:21 +0100

    Redirect to UserJourney if it exists (#15)

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,5 +1,10 @@
 class JourneysController < ApplicationController
   def show
     @journey = Journey.find_by!(slug: params[:id])
+
+    if current_user
+      @user_journey = UserJourney.find_by(user: current_user, journey: @journey)
+      redirect_to @user_journey, notice: 'Vitajte späť!' if @user_journey.present?
+    end
   end
 end
---
+++ spec/features/journeys_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.feature "Journeys", type: :feature do
+  pending 'As a returning user I want to see my UserJourney when I click on a Journey'
+end
