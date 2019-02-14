0a603d504978bb04ed06051e764d60d517c9895c
Miroslav Hettes <mirrec@gmail.com>
2019-02-14 16:21:53 +0100

    Do not redirect to last user_journey, but show it on click

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,10 @@ class ApplicationController < ActionController::Base
       redirect_to new_session_path
     end
   end
+
+  def load_last_unfinished_journey(user, journey)
+    if user
+      @unfinished_user_journey = UserJourney.order(id: :desc).find_by(user: user, journey: journey)
+    end
+  end
 end
--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,10 +1,6 @@
 class JourneysController < ApplicationController
   def show
     @journey = Journey.published.find_by!(slug: params[:id])
-
-    if current_user
-      @user_journey = UserJourney.find_by(user: current_user, journey: @journey)
-      redirect_to @user_journey if @user_journey.present?
-    end
+    load_last_unfinished_journey(current_user, @journey)
   end
 end
