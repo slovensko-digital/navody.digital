e3d0eddaa62710dcc6159e63be4778fbb3164c26
Miroslav Hettes <mirrec@gmail.com>
2019-02-14 17:40:55 +0100

    Add link to next step

--- app/controllers/user_journeys_controller.rb
+++ app/controllers/user_journeys_controller.rb
@@ -4,6 +4,7 @@ class UserJourneysController < ApplicationController
   def show
     @user_journey = current_user.user_journeys.find(params[:id])
     @user_step_by_steps = @user_journey.user_steps.index_by { |user_step| user_step.step }
+    @next_step = @user_journey.journey.steps.order(:position).first
   end
 
   def start
