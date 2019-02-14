9e1eb3bf80f9c91a3a68a26f51b29e28814033dd
Tomas Radic <tomas.radic@gmail.com>
2019-02-14 16:02:50 +0100

    Issue #13 | Removed unused code

--- spec/controllers/user_steps_controller_spec.rb
+++ spec/controllers/user_steps_controller_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe UserStepsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      # user_step = create(:user_step)
       user_step.step.update_attribute(:journey, user_step.user_journey.journey)
 
       get :show, params: { user_journey_id: user_step.user_journey.id, id: user_step.step.to_param }, session: { user_id: user_step.user_journey.user.id }
