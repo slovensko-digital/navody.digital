839983fdb637dcea6250e6626a5c3554ad8b4f26
jsuchal <jan.suchal@gmail.com>
2019-04-11 18:22:18 +0200

    Fix old artefacts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,6 @@ class PagesController < ApplicationController
   def index
     @faqs = Page.faq.all # TODO: fetch top FAQs here
     @journeys = Journey.published
-    if current_user
-      @user_journeys = current_user.user_journeys.includes(:journey).order(id: :desc)
-    end
   end
 
   def show
