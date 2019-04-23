866b542fa3c4bff6e6ac86c6db5a8da0ae2967dd
Ernest Walzel <ewalzel@gmail.com>
2019-04-23 13:00:42 +0200

    OpenGraph (OG) support Set journey image for FB share. Related to #156.

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -2,6 +2,8 @@ class JourneysController < ApplicationController
   def show
     @journey = Journey.published.find_by!(slug: params[:id])
     @next_step = @journey.steps.order(:position).first
+
+    set_journey_page_metadata(@journey)
     load_newest_user_journey(current_user, @journey)
   end
 end
