e556dca025db09ffe4120b780d2489317fad3991
jsuchal <jan.suchal@gmail.com>
2019-05-01 20:08:59 +0200

    This partially reverts commit 86b9a98c

--- spec/rake/sitemap_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-require 'rake'
-
-describe 'foo namespace rake task' do
-  it 'runs successfully' do
-    Rake.application.rake_require "tasks/bar"
-    Rake::Task["sitemap:create"].invoke
-  end
-end
