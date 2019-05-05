add2554aab4eaafb2af8726c3342a1d63453e0d7
marosj <marosjgithub@gmail.com>
2019-05-05 09:25:43 +0200

    Errors instead of has_errors Id for error message Correction for label "for" Class for field accepts option from caller

--- app/helpers/app_form_builder.rb
+++ app/helpers/app_form_builder.rb
@@ -1,17 +1,30 @@
 class AppFormBuilder < ActionView::Helpers::FormBuilder
 
   def text_field(method, options={})
-    has_errors = @object.errors[method].any?
+    errors = @object.errors[method]
+    group_classes = ['govuk-form-group', errors.any? ? 'govuk-form-group--error' : nil]
+    field_classes = ['govuk-input', errors.any? ? 'govuk-input--error': nil, options[:class]]
 
-    @template.content_tag(:div, class: 'govuk-form-group' + (has_errors ? ' govuk-form-group--error' : '')) do
-      #TODO label as option or parameter?
-      @template.label(:label, method, options[:label], class: 'govuk-label') +
-        (options[:hint] ? @template.content_tag(:span, options[:hint], class: 'govuk-hint') : '') +
-      #TODO better wrapping for errors - see design manual. There should be id. Add span with visually-hidden.
-      # Joint with '. ' or repeat span?
-        (has_errors ? @template.content_tag(:span, @object.errors[method].join(' '), class: 'govuk-error-message') : '') +
-        super(method, options.merge({class: 'govuk-input' + (has_errors ? ' govuk-input--error' : '')}))
-      #TODO not use options.merge but objectify_options and other merge possibility for class
+    label = options.delete(:label)
+    label = label ? @template.label(@object_name, method, label, class: 'govuk-label') : ''
+
+    hint = options.delete(:hint)
+    hint = hint ? @template.content_tag(:span, hint, class: 'govuk-hint') : ''
+
+    #TODO better way to get/create id - it might not work for object from array (with index)
+    error_id = @object_name + '_' + method.to_s + '-error'
+    errors_msg = @template.content_tag :span, id: error_id, class: 'govuk-error-message' do
+      errors.each do |e|
+        @template.concat @template.content_tag(:span, 'Chyba: ', class: 'govuk-visually-hidden')
+        @template.concat e
+      end
+    end
+
+    @template.content_tag(:div, class: group_classes) do
+      @template.concat label
+      @template.concat hint
+      @template.concat errors.any? ? errors_msg : ''
+      @template.concat super(method, options.merge({class: field_classes}))
     end
   end
 
