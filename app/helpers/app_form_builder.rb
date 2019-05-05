b2f56b777693df2a588d949dda8425b5a2cab8e1
marosj <marosjgithub@gmail.com>
2019-05-05 15:21:58 +0200

    Radio button

--- app/helpers/app_form_builder.rb
+++ app/helpers/app_form_builder.rb
@@ -1,9 +1,9 @@
 class AppFormBuilder < ActionView::Helpers::FormBuilder
 
-  def text_field(method, options={})
+  def text_field(method, options = {})
     errors = @object.errors[method]
     group_classes = ['govuk-form-group', errors.any? ? 'govuk-form-group--error' : nil]
-    field_classes = ['govuk-input', errors.any? ? 'govuk-input--error': nil, options[:class]]
+    field_classes = ['govuk-input', errors.any? ? 'govuk-input--error' : nil, options[:class]]
 
     label = options.delete(:label)
     label = label ? @template.label(@object_name, method, label, class: 'govuk-label') : ''
@@ -11,20 +11,27 @@ class AppFormBuilder < ActionView::Helpers::FormBuilder
     hint = options.delete(:hint)
     hint = hint ? @template.content_tag(:span, hint, class: 'govuk-hint') : ''
 
-    #TODO better way to get/create id - it might not work for object from array (with index)
-    error_id = @object_name + '_' + method.to_s + '-error'
-    errors_msg = @template.content_tag :span, id: error_id, class: 'govuk-error-message' do
-      errors.each do |e|
-        @template.concat @template.content_tag(:span, 'Chyba: ', class: 'govuk-visually-hidden')
-        @template.concat e
-      end
-    end
-
     @template.content_tag(:div, class: group_classes) do
       @template.concat label
       @template.concat hint
-      @template.concat errors.any? ? errors_msg : ''
+      @template.concat error_message(method)
       @template.concat super(method, options.merge({class: field_classes}))
+    end
+  end
+
+  def radio_button(method, tag_value, options = {})
+    field_classes = ['govuk-radios__input', options[:class]]
+
+    label = options.delete(:label)
+    label = label ? @template.label(@object_name, method.to_s + "_" + tag_value, label, class: 'govuk-label govuk-radios__label') : ''
+
+    hint = options.delete(:hint)
+    hint = hint ? @template.content_tag(:span, hint, class: 'govuk-hint govuk-radios__hint') : ''
+
+    @template.content_tag(:div, class: 'govuk-radios__item') do
+      @template.concat super(method, tag_value, options.merge({class: field_classes}))
+      @template.concat label
+      @template.concat hint
     end
   end
 
@@ -32,4 +39,20 @@ class AppFormBuilder < ActionView::Helpers::FormBuilder
     super(value, options.merge({class: ['govuk-button', options[:class]]}))
   end
 
+  def error_message(method)
+    errors = @object.errors[method]
+    if errors.any?
+      #TODO better way to get/create id - it might not work for object from array (with index)
+      error_id = @object_name + '_' + method.to_s + '-error'
+      @template.content_tag :span, id: error_id, class: 'govuk-error-message' do
+        errors.each do |e|
+          @template.concat @template.content_tag(:span, 'Chyba: ', class: 'govuk-visually-hidden')
+          @template.concat e
+        end
+      end
+    else
+      ''
+    end
+
+  end
 end
