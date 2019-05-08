dcbc14c36e0636dc0fd8d677f1bb3c57c66a5ae7
marosj <marosjgithub@gmail.com>
2019-05-08 09:21:42 +0200

    Refactoring

--- app/helpers/app_form_builder.rb
+++ app/helpers/app_form_builder.rb
@@ -1,21 +1,25 @@
 class AppFormBuilder < ActionView::Helpers::FormBuilder
 
   def text_field(method, options = {})
-    errors = @object.errors[method]
-    group_classes = ['govuk-form-group', errors.any? ? 'govuk-form-group--error' : nil]
-    field_classes = ['govuk-input', errors.any? ? 'govuk-input--error' : nil, options[:class]]
+    has_errors = @object.errors[method].present?
+
+    group_classes = ['govuk-form-group']
+    group_classes << 'govuk-form-group--error' if has_errors
+
+    field_classes = ['govuk-input', options[:class]]
+    field_classes << 'govuk-input--error' if has_errors
 
     label = options.delete(:label)
-    label = label ? @template.label(@object_name, method, label, class: 'govuk-label') : ''
+    label = label(method, label, class: 'govuk-label') if label
 
     hint = options.delete(:hint)
-    hint = hint ? @template.content_tag(:span, hint, class: 'govuk-hint') : ''
+    hint = @template.content_tag(:span, hint, class: 'govuk-hint') if hint
 
     @template.content_tag(:div, class: group_classes) do
       @template.concat label
       @template.concat hint
       @template.concat error_message(method)
-      @template.concat super(method, options.merge({class: field_classes}))
+      @template.concat super(method, objectify_options(options.merge({class: field_classes})))
     end
   end
 
@@ -23,20 +27,20 @@ class AppFormBuilder < ActionView::Helpers::FormBuilder
     field_classes = ['govuk-radios__input', options[:class]]
 
     label = options.delete(:label)
-    label = label ? @template.label(@object_name, method.to_s + "_" + tag_value, label, class: 'govuk-label govuk-radios__label') : ''
+    label = label(method.to_s + "_" + tag_value, label, class: 'govuk-label govuk-radios__label') if label
 
     hint = options.delete(:hint)
-    hint = hint ? @template.content_tag(:span, hint, class: 'govuk-hint govuk-radios__hint') : ''
+    hint = @template.content_tag(:span, hint, class: 'govuk-hint govuk-radios__hint') if hint
 
     @template.content_tag(:div, class: 'govuk-radios__item') do
-      @template.concat super(method, tag_value, options.merge({class: field_classes}))
+      @template.concat super(method, tag_value, objectify_options(options.merge({class: field_classes})))
       @template.concat label
       @template.concat hint
     end
   end
 
   def submit(value = nil, options = {})
-    super(value, options.merge({class: ['govuk-button', options[:class]]}))
+    super(value, objectify_options(options.merge({class: ['govuk-button', options[:class]]})))
   end
 
   def error_message(method)
