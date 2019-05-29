99a90b60ebd37067ceb5c10f7f6425981b4a01f6
Stanley Modrak <stanleysmith@tutanota.de>
2019-05-29 23:28:50 +0200

    Ran 'rails db:migrate'

--- db/structure.sql
+++ db/structure.sql
@@ -40,7 +40,8 @@ CREATE TABLE public.journeys (
     description text NOT NULL,
     "position" integer DEFAULT 0 NOT NULL,
     featured_position integer DEFAULT 0 NOT NULL,
-    image_name text
+    image_name text,
+    custom_title character varying
 );
 
 
@@ -241,7 +242,8 @@ CREATE TABLE public.steps (
     "position" integer DEFAULT 0 NOT NULL,
     app_url character varying,
     type character varying DEFAULT 'BasicStep'::character varying NOT NULL,
-    app_link_text character varying
+    app_link_text character varying,
+    custom_title character varying
 );
 
 
@@ -845,6 +847,8 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20190411104731'),
 ('20190423124647'),
 ('20190423214925'),
-('20190424074855');
+('20190424074855'),
+('20190529210530'),
+('20190529210630');
 
 
