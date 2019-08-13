288138fa6f6fc839f3033c8676cafa925b013f0f
Stanley Modrak <stanislav.modrak@gmail.com>
2019-08-13 19:18:39 +0200

    Add duplicate method with diff return type

--- app/helpers/notification_subscriptions_helper.rb
+++ app/helpers/notification_subscriptions_helper.rb
@@ -2,6 +2,12 @@ module NotificationSubscriptionsHelper
   def render_notification_subscription_component(subscription_types)
     group = NotificationSubscriptionGroup.new(subscription_types: subscription_types)
     group.user = current_user
+    render partial: 'notification_subscriptions/form', object: group
+  end
+
+  def render_notification_subscription_component_to_sting(subscription_types)
+    group = NotificationSubscriptionGroup.new(subscription_types: subscription_types)
+    group.user = current_user
     render_to_string partial: 'notification_subscriptions/form', object: group
   end
 end
