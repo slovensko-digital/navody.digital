85476df49c19698874c050d5f97c9c155dc9b401
Stanley Modrak <stanislav.modrak@gmail.com>
2019-08-29 22:59:31 +0200

    Correct syntax

--- spec/features/embedded_apps_spec.rb
+++ spec/features/embedded_apps_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature "Embedded apps" do
-  let!(:journey) { create(:journey) }
-  let!(:child_birth_step) { create(:step, journey: journey, description: '<embedded-app app-id="narodenie-rodny-list" />') }
+  let!(:journey) {create(:journey)}
+  let!(:child_birth_step) {create(:step, journey: journey, description: '<embedded-app app-id="narodenie-rodny-list" />')}
 
   scenario 'As a user I want to see an the Child Birth Picking up Protocol app' do
     visit journey_step_path(journey, child_birth_step)
