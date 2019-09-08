522f78a146e11d0dc22b7187b9e8b6afcbd90791
Stanley Modrak <44023416+smith558@users.noreply.github.com>
2019-09-08 22:03:40 +0200

    Update app/helpers/notification_subscriptions_helper.rb
    Co-Authored-By: Ernest Walzel <eronisko@users.noreply.github.com>

--- app/helpers/notification_subscriptions_helper.rb
+++ app/helpers/notification_subscriptions_helper.rb
@@ -3,6 +3,6 @@ module NotificationSubscriptionsHelper
     group = NotificationSubscriptionGroup.new(subscription_types: subscription_types)
     group.user = current_user
     group.journey = journey
-    render partial: 'notification_subscriptions/form', object: group, locals: {extra_attributes: extra_attributes}
+    render partial: 'notification_subscriptions/form', object: group, locals: { extra_attributes: extra_attributes }
   end
 end
