d81480a04bac702f0214a82661cee6767175b499
peter <Peter Toth>
2019-09-14 19:37:07 +0200

    Remove cookie consent from admin

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( apps/ep_vote_app/libs.js mailer.scss )
+Rails.application.config.assets.precompile += %w( admin_application.js )
