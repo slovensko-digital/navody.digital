68d8a7705f9c8c4b99bc928c1ae75922671c67cd
Jano Suchal <jan.suchal@slovensko.digital>
2019-09-26 22:05:57 +0200

    Rails 6 breaking change fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,8 @@ OmniAuth.config.logger = Rails.logger
 OmniAuth.config.failure_raise_out_environments = []
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  google_client_id = Rails.application.config_for(:auth).dig('google', 'client_id')
-  google_client_secret = Rails.application.config_for(:auth).dig('google', 'client_secret')
+  google_client_id = Rails.application.config_for(:auth).dig(:google, :client_id)
+  google_client_secret = Rails.application.config_for(:auth).dig(:google, :client_secret)
 
   provider :google_oauth2, google_client_id, google_client_secret
   provider :magic_link, {
