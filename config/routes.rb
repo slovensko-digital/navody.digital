226681be953592eadb20e3a4bdb805d04468b677
jsuchal <jan.suchal@gmail.com>
2019-11-13 11:25:44 +0100

    Fix route name

--- config/routes.rb
+++ config/routes.rb
@@ -68,7 +68,7 @@ Rails.application.routes.draw do
             get :end, path: 'volte-zodpovedne'
             get :home, path: 'hlasovanie-v-mieste-trvaleho-bydliska'
             get :person, path: 'hlasovaci-preukaz-osobne'
-            get :non_sk_nationality, path: 'hlasovanie-obcanov-inej-narodnosti'
+            get :non_sk_nationality, path: 'hlasovanie-neobcana'
             get :delivery, path: 'hlasovaci-preukaz'
             post :delivery, path: 'hlasovaci-preukaz'
             get :world, path: 'hlasovanie-v-zahranici'
