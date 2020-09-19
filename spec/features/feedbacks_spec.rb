4d68f4c77820d1f05d632e60d968b55c5195ac00
janmacek <macek.1991@gmail.com>
2020-09-19 16:54:17 +0200

    Added feature tests na Feedback Bar (100% coverage) (#375)
    Added feature tests na Feedback Bar, closes #162 
    
    Co-authored-by: Jan Macek <jan.macek@eset.sk>
    Co-authored-by: Jano Suchal <jan.suchal@slovensko.digital>

---
+++ spec/features/feedbacks_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature 'Feedback Bar', type: :feature, js: true do
+
+  scenario 'User opens feedback bar' do
+    visit root_path
+    find('#ga-feedback-bug-report').click
+    
+    expect(page.body).to have_text('Nahlásenie chyby')
+  end
+
+  scenario 'User submits feedback' do
+    visit root_path
+    find('#ga-feedback-bug-report').click
+    fill_in 'bug_what_were_you_doing', with: 'Testing feedback bar'
+    fill_in 'bug_what_went_wrong', with: 'Testing of feedback bar'
+    expect_any_instance_of(Recaptcha::Adapters::ControllerMethods).to receive(:verify_recaptcha).and_return(true)
+    click_button 'Odoslať'
+    
+    expect(page).to have_text('Váš podnet bol odoslaný. Ďakujeme.')
+  end
+
+  scenario 'User submits feedback and Recaptcha is requested' do
+    visit root_path
+    find('#ga-feedback-bug-report').click
+    fill_in 'bug_what_were_you_doing', with: 'Testing feedback bar'
+    fill_in 'bug_what_went_wrong', with: 'Testing of feedback bar'
+    expect_any_instance_of(Recaptcha::Adapters::ControllerMethods).to receive(:verify_recaptcha).and_return(false)
+    click_button 'Odoslať'
+    
+    expect(page).to have_text('Prosím, potvrďte, že nie ste robot.')
+  end
+end
