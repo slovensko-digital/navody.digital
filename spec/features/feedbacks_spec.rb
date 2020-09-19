ce31aa8cb8eb2b55982383adc835b8e692516ac4
Jano Suchal <jan.suchal@slovensko.digital>
2020-09-19 16:50:37 +0200

    Update spec/features/feedbacks_spec.rb

--- spec/features/feedbacks_spec.rb
+++ spec/features/feedbacks_spec.rb
@@ -21,12 +21,13 @@ RSpec.feature 'Feedback Bar', type: :feature, js: true do
   end
 
   scenario 'User submits feedback and Recaptcha is requested' do
-    visit '/'
+    visit root_path
     find('#ga-feedback-bug-report').click
     fill_in 'bug_what_were_you_doing', with: 'Testing feedback bar'
     fill_in 'bug_what_went_wrong', with: 'Testing of feedback bar'
     expect_any_instance_of(Recaptcha::Adapters::ControllerMethods).to receive(:verify_recaptcha).and_return(false)
     click_button 'Odoslať'
+    
     expect(page).to have_text('Prosím, potvrďte, že nie ste robot.')
   end
 end
