26b04716e2ad311a1646bbff11d6ca3b755ef503
jsuchal <jan.suchal@gmail.com>
2021-03-22 22:39:29 +0100

    Cleanup more

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,16 +7,9 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :type, presence: true, inclusion: Task::TYPES
 
-  validates :url, url: true, if: :external_link_task?
-  validates :url_title, presence: true, if: :external_link_task?
+  validates :url, url: true, if: -> { type == 'ExternalLinkTask' }
 
   default_scope do
     order(position: :asc, id: :asc)
-  end
-
-  private
-
-  def external_link_task?
-    type == 'ExternalLinkTask'
   end
 end
