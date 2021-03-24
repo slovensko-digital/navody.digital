aad8f209e4a5855100bc1a55a734be608b48d43e
michal-rohacek <michal.rohacek.256@gmail.com>
2021-03-24 11:55:45 +0100

    Revert some changes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,4 @@
 class Task < ApplicationRecord
-  before_save :normalize_blank_attributes
-
   self.inheritance_column = nil
   TYPES = %w[SimpleTask ExternalLinkTask]
 
@@ -9,7 +7,6 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :type, presence: true, inclusion: Task::TYPES
   validates :url, url: true, if: -> { type == 'ExternalLinkTask' }
-  validates :url_title, presence: true, allow_nil: true
 
   default_scope do
     order(position: :asc, id: :asc)
