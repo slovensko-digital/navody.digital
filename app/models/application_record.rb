b60badb1733338e482844268fac6ab3be687861e
michal-rohacek <michal.rohacek.256@gmail.com>
2021-03-24 11:47:08 +0100

    Add normalization of strings and validation

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def normalize_blank_attributes
+    attributes.keys.each do |column|
+      self[column] = nil if self[column]&.strip.blank?
+    end
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  before_save :normalize_blank_attributes
+
   self.inheritance_column = nil
   TYPES = %w[SimpleTask ExternalLinkTask]
 
@@ -7,6 +9,7 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :type, presence: true, inclusion: Task::TYPES
   validates :url, url: true, if: -> { type == 'ExternalLinkTask' }
+  validates :url_title, presence: true, allow_nil: true
 
   default_scope do
     order(position: :asc, id: :asc)
