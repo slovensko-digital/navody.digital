0d30aa130a94aab2214e3ab5f4b2cd470dec74aa
jsuchal <jan.suchal@gmail.com>
2022-04-07 15:34:28 +0200

    Refactor a bit

--- app/services/uploads_service.rb
+++ app/services/uploads_service.rb
@@ -1,43 +1,41 @@
 class UploadsService
-  class << self
-    def local_storage?
-      not ActiveStorage::Blob.service.respond_to? :bucket
-    end
 
-    def get_files(prefix: nil)
-      if local_storage?
-        scope = ActiveStorage::Blob
-        scope = scope.where('key ILIKE ?', "#{prefix}%") if prefix
-        scope.all
-      else
-          ActiveStorage::Blob.service.bucket.objects(prefix: prefix)
-      end
-    end
+  def self.local_storage?
+    !ActiveStorage::Blob.service.respond_to?(:bucket)
+  end
 
-    def get_url(file)
-      if local_storage?
-        Rails.application.routes.url_helpers.rails_blob_path(file, only_path: true)
-      else
-        file.public_url
-      end
+  def self.find_files(prefix: nil)
+    if local_storage?
+      scope = ActiveStorage::Blob
+      scope = scope.where('key ILIKE ?', "#{prefix}%") if prefix
+      scope.all
+    else
+      ActiveStorage::Blob.service.bucket.objects(prefix: prefix)
     end
+  end
 
-    def get_url_for_blob(blob)
-      if local_storage?
-        Rails.application.routes.url_helpers.rails_blob_path(blob, only_path: true)
-      else
-        blob.service.bucket.url + '/' + blob.key
-      end
+  def self.get_url(file)
+    if local_storage?
+      Rails.application.routes.url_helpers.rails_blob_path(file, only_path: true)
+    else
+      file.public_url
     end
+  end
 
-    def delete_file(filename)
-      if local_storage?
-        ActiveStorage::Blob.find_by(key: filename).purge
-      else
-        temp = ActiveStorage::Blob.find_by(key: filename)
-        temp ? temp.purge : nil
-        ActiveStorage::Blob.service.delete(filename)
-      end
+  def self.get_url_for_blob(blob)
+    if local_storage?
+      Rails.application.routes.url_helpers.rails_blob_path(blob, only_path: true)
+    else
+      blob.service.bucket.url + '/' + blob.key
+    end
+  end
+
+  def self.delete_file(filename)
+    if local_storage?
+      ActiveStorage::Blob.find_by(key: filename).purge
+    else
+      ActiveStorage::Blob.find_by(key: filename).try(&:purge)
+      ActiveStorage::Blob.service.delete(filename)
     end
   end
 end
