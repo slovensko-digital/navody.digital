0aa7fca68625bf60d2de31f87a8177f50b1f665e
Michal Barla <michal.barla@gmail.com>
2022-06-25 16:48:55 +0200

    Add CompanyRegister Stakeholder Id Check Job

--- app/models/my/company_register_stakeholder_id_check/job.rb
+++ app/models/my/company_register_stakeholder_id_check/job.rb
@@ -1,8 +1,23 @@
 module My
   module CompanyRegisterStakeholderIdCheck
+    class ORSRRecordError < StandardError; end
+
     class Job < ApplicationJob
       def perform(company, journey)
-        # mihal
+        ico = company.ico
+        check_result = ORSRRecordChecker.call(ico)
+        raise ORSRRecordError("Several active records found for one company id: #{ico}") if check_result.size > 1
+
+        check_result = check_result.first
+
+        entry = FeedEntry.find_or_initialize_by(user: company.user, thing: company, identifier: ico, type: 'CompanyRegisterStakeholderIdCheckFeedEntry') do |entry|
+          entry.deadline_at = Date.new(2022, 9, 30)
+          entry.journey = journey
+        end
+
+        entry.last_checked_at = Time.now
+        entry.custom_fields = check_result
+        entry.status = check_result[:missing].any? ? 'bad' : 'ok'
       end
     end
   end
