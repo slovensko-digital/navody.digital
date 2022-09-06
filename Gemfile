27a0f296b2bea78ec30de36a5751422027eb2eee
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2022-09-06 11:30:50 +0200

    Configure sitemap s3 upload

--- Gemfile
+++ Gemfile
@@ -67,6 +67,8 @@ gem 'sib-api-v3-sdk'
 gem 'recaptcha'
 gem 'friendly_id'
 
+gem 'fog-aws'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,20 @@
 SitemapGenerator::Sitemap.default_host = "https://navody.digital"
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
+sitemap_config = Rails.application.config_for(:sitemap)
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
+  aws_access_key_id: sitemap_config[:access_key_id],
+  aws_secret_access_key: sitemap_config[:secret_access_key],
+  fog_provider: 'AWS',
+  fog_public: true,
+  fog_region: sitemap_config[:region],
+  fog_directory: sitemap_config[:bucket],
+  fog_path_style: sitemap_config[:path_style],
+  fog_storage_options: {
+    endpoint: sitemap_config[:endpoint],
+  },
+)
+
 SitemapGenerator::Sitemap.create do
   Journey.find_each do |journey|
     add journey_path(journey.slug), lastmod: journey.updated_at
