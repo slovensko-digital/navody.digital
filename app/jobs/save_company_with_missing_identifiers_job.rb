4fc45584749aa9191a90ff6014eebc93283c210f
luciajanikova <19lucia99@gmail.com>
2022-09-08 23:10:38 +0200

    Fix submission extra params

--- app/jobs/save_company_with_missing_identifiers_job.rb
+++ app/jobs/save_company_with_missing_identifiers_job.rb
@@ -2,12 +2,12 @@ class SaveCompanyWithMissingIdentifiersJob < ApplicationJob
   queue_as :default
 
   def perform(submission)
-    cin = submission.extra['company_cin']
+    cin = submission.extra.dig('params', 'company_cin')
 
     company_record = Apps::OrSrApp::StakeholdersIdentifiers::CompanyRecord.find_or_create_by({
       cin: Integer(cin)
     })
 
-    company_record.email = submission.user_email
+    company_record.update(:email => submission.user_email)
   end
 end
