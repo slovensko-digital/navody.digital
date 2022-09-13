cb82bd8a9482da6f98b71497ce33e177b5c0560a
Marek Ceľuch <celuchmarek@gmail.com>
2022-09-13 23:54:17 +0200

    update categories controller to cope with updated model

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find_by!(id: params[:id])
-    @documents = Document.featured.includes(:searchable).order(featured_position: :asc).map(&:searchable).select{ |d| d.categories.include?(@category) }.compact
+    @documents = Document.featured.searchable_with_category(@category).order(featured: :desc, featured_position: :asc).map{ |d| d.searchable }
   end
 end
