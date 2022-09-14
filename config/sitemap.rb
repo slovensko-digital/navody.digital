a810e9f80da7b15552e00268a3c754aabf7485b0
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2022-09-14 09:14:56 +0200

    Scope down Journeys and Pages which are published to sitemap.xml (#507)
    Co-authored-by: Jano Suchal <jan.suchal@gmail.com>

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -13,14 +13,14 @@ SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(sitemap_
 )
 
 SitemapGenerator::Sitemap.create do
-  Journey.find_each do |journey|
+  Journey.displayable.find_each do |journey|
     add journey_path(journey.slug), lastmod: journey.updated_at
     journey.steps.find_each do |step|
       add journey_step_path(journey, step), lastmod: step.updated_at
     end
   end
 
-  Page.find_each do |page|
+  Page.searchable.find_each do |page|
     if page.is_faq?
       add faq_path(page.slug), lastmod: page.updated_at
     else
