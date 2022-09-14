7c86de595f87c73b1a2773fa38ef5b1fef059c00
Marek Ceľuch <celuchmarek@gmail.com>
2022-09-14 10:05:37 +0200

    update spec factories with short_description

--- spec/factories/app.rb
+++ spec/factories/app.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     end
     slug { title.parameterize }
     published_status { 'PUBLISHED' }
+    short_description { Faker::Lorem.paragraph(sentence_count: 1) }
     sequence(:image_name) do |n|
       '%s %d' % [Faker::Commerce.product_name, n]
     end
