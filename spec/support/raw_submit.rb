429b3b4c432992010ed3efd5545cea197b151eb3
Jan Suchal <jan.suchal@gmail.com>
2023-03-24 11:18:06 +0100

    Bump ruby & gems

--- spec/support/raw_submit.rb
+++ spec/support/raw_submit.rb
@@ -3,3 +3,12 @@ class Capybara::Session
     Capybara::RackTest::Form.new(driver, element.native).submit({})
   end
 end
+
+# monkey patch submit method to match 3 parameters
+class Capybara::RackTest::Form < Capybara::RackTest::Node
+  def submit(button)
+    action = button&.[]('formaction') || native['action']
+    method = button&.[]('formmethod') || request_method
+    driver.submit(method, action.to_s, params(button))
+  end
+end
