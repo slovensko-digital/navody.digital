22290121ce95ed83e2d79558c06443f1a9b0165a
Miroslav Hettes <mirrec@gmail.com>
2023-03-25 16:43:08 +0100

    Move background setup to one scenario scope

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,32 +1,32 @@
 require 'rails_helper'
 
 RSpec.feature 'Search', type: :feature do
-  background do
-    @matching = 'Batman'
-    unmatching = 'Superman'
-    @matching_journey = create(:journey, title: "#{@matching} journey")
-    @matching_step = create(:step, title: "#{@matching} step", journey: @matching_journey)
-    @unmatching_step = create(:step, title: "#{unmatching} step", journey: @matching_journey)
-    @unmatching_journey = create(:journey, title: "#{unmatching} journey")
-    @unmatching_journey_matching_step = create(:journey, title: "#{unmatching} 2nd journey")
-    @matching_step2 = create(:step, title: "#{@matching} step2", journey: @unmatching_journey_matching_step)
-    @matching_faq = create(:page, title: "#{@matching} faq", is_faq: true)
-    @unmatching_faq = create(:page, title: "#{unmatching} faq", is_faq: true)
-  end
-
   scenario 'Visitor searches for topic of interest' do
+    matching = 'Batman'
+    unmatching = 'Superman'
+    
+    matching_journey = create(:journey, title: "#{matching} journey")
+    matching_step = create(:step, title: "#{matching} step", journey: matching_journey)
+    unmatching_step = create(:step, title: "#{unmatching} step", journey: matching_journey)
+    unmatching_journey = create(:journey, title: "#{unmatching} journey")
+    unmatching_journey_matching_step = create(:journey, title: "#{unmatching} 2nd journey")
+    matching_step2 = create(:step, title: "#{matching} step2", journey: unmatching_journey_matching_step)
+    matching_faq = create(:page, title: "#{matching} faq", is_faq: true)
+    unmatching_faq = create(:page, title: "#{unmatching} faq", is_faq: true)
+
     visit root_path
-    fill_in 'q', with: @matching
+    fill_in 'q', with: matching
     click_button 'Hľadať'
+
     expect(page).to have_text('Výsledky vyhľadávania')
-    expect(page).to have_text(@matching_journey.title)
-    expect(page).to have_text(@matching_step.title)
-    expect(page).to have_text(@matching_faq.title)
-    expect(page).to have_text(@unmatching_journey_matching_step.title)
-    expect(page).to have_text(@matching_step2.title)
-    expect(page).not_to have_text(@unmatching_journey.title)
-    expect(page).not_to have_text(@unmatching_step.title)
-    expect(page).not_to have_text(@unmatching_faq.title)
+    expect(page).to have_text(matching_journey.title)
+    expect(page).to have_text(matching_step.title)
+    expect(page).to have_text(matching_faq.title)
+    expect(page).to have_text(unmatching_journey_matching_step.title)
+    expect(page).to have_text(matching_step2.title)
+    expect(page).not_to have_text(unmatching_journey.title)
+    expect(page).not_to have_text(unmatching_step.title)
+    expect(page).not_to have_text(unmatching_faq.title)
   end
 
   scenario 'Not found results' do
@@ -44,7 +44,7 @@ RSpec.feature 'Search', type: :feature do
     non_searchable_page =create(:page, title: "#{matching} non faq and not searchable", is_faq: false, is_searchable: false)
 
     visit root_path
-    fill_in 'q', with: @matching
+    fill_in 'q', with: matching
     click_button 'Hľadať'
 
     expect(page).to have_text('Výsledky vyhľadávania')
