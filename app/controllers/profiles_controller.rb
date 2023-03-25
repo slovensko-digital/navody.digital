520613e5b0056e6e8b72b381b8a1178b6cb4047a
Jan Suchal <jan.suchal@gmail.com>
2023-03-25 14:46:54 +0100

    Copy

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,15 @@
 class ProfilesController < ApplicationController
-    before_action :require_user
+  before_action :require_user
+  before_action :disable_feedback
 
-    def show # shows the profile page of currently logged user
-        @user_email = current_user.email
-    end
+  def show # shows the profile page of currently logged user
+    @user_email = current_user.email
+  end
 
-    def destroy # deletes user profile
-        user  = current_user
-        reset_session
-        user.destroy
-        redirect_to root_path, notice: 'Odstránenie konta bolo úspešné.'
-    end
+  def destroy # deletes user profile
+    user = current_user
+    reset_session
+    user.destroy
+    redirect_to root_path, notice: 'Odstránenie konta bolo úspešné.'
+  end
 end
