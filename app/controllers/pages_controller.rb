6bca363bafcaa7e5a1a898615ffcfe42f6697593
Jano Suchal <jan.suchal@slovensko.digital>
2023-03-25 11:19:40 +0100

    Update app/controllers/pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     @metadata.og.title = 'Návody.Digital: Jednoduché návody na slovenské úrady'
     @metadata.og.description = 'Interaktívne návody, ako vybaviť úradné záležitosti elektronicky.'
 
-    @faqs = Page.faq.all.order(position: :asc) # TODO: fetch top FAQs here
+    @faqs = Page.faq.order(position: :asc).all
     @categories = Category.featured.order(featured_position: :asc, name: :asc)
     @documents = Document.featured.includes(:searchable).order(featured_position: :asc).map(&:searchable).compact # we do not have FK constraints here, compact eliminates any possible documents hanging without searchable counterpart, which was deleted
   end
