08f54af453dcb9e0a9eb71eafdadc97e1d6868d8
Marek Ceľuch <celuchmarek@gmail.com>
2023-05-29 10:24:00 +0200

    change dates to make the app active

--- app/models/apps/parliament_vote_app/application_form.rb
+++ app/models/apps/parliament_vote_app/application_form.rb
@@ -1,11 +1,11 @@
 module Apps
   module ParliamentVoteApp
     class ApplicationForm
-      VOTE_DATE = Date.new(2020, 2, 29)
-      DELIVERY_BY_POST_DEADLINE_DATE = Date.new(2020, 2, 10)
-      PICKUP_DEADLINE_DATE = Date.new(2020, 2, 28)
-      VOTE_BY_POST_DEADLINE_DATE = Date.new(2020, 1, 10)
-      REQUEST_SENDING_DEADLINE_DATE = Date.new(2020, 2, 10)
+      VOTE_DATE = Date.new(2024, 2, 29)
+      DELIVERY_BY_POST_DEADLINE_DATE = Date.new(2024, 2, 10)
+      PICKUP_DEADLINE_DATE = Date.new(2024, 2, 28)
+      VOTE_BY_POST_DEADLINE_DATE = Date.new(2024, 1, 10)
+      REQUEST_SENDING_DEADLINE_DATE = Date.new(2024, 2, 10)
 
       include ActiveModel::Model
 
