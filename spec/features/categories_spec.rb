1a88f0a20f97a162b09256eb7c5faaf79ed6e71f
Jan Suchal <jan.suchal@gmail.com>
2023-10-07 09:25:36 +0200

    Refactor a bit

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'Categories', type: :feature do
-  scenario 'Visitor see category detail' do
+  scenario 'As a visitor I can see a journey on a category page' do
     published_journey = create(:journey, title: "published journey", published_status: 'PUBLISHED')
 
     categorization = create(:categorization, categorizable: published_journey)
