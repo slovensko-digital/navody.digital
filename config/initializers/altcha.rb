752d4afa902f8aa13afb7c1f217066f8498b9774
Michal Barla <michal.barla@gmail.com>
2024-11-16 13:10:51 +0100

    Make the altcha challenge a lot easier

--- config/initializers/altcha.rb
+++ config/initializers/altcha.rb
@@ -2,7 +2,7 @@
 
 Altcha.setup do |config|
   config.algorithm = 'SHA-256'
-  config.num_range = (50_000..300_000)
+  config.num_range = (50_000..51_000)
   config.timeout = 5.minutes
   config.hmac_key = 'dfa06d467a84fea13941f1c52c38c6458a67617a'
 end
